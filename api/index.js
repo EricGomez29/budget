fa76b7e5074d631f3fb9a030909ac9fc70f70b0e
EricGomez29 <eric.gomez.unq@gmail.com>
2021-01-14 17:33:01 -0300

    Limpiando carpeta client de cosas adicionales que trae 'npx create-react-app'

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 
 const force = true;
 conn.sync({ force }).then(() => {
-  server.listen(3000, () => {
-    console.log('%s listening at 3000');
+  server.listen(3001, () => {
+    console.log('%s listening at 3001');
   })
 });
